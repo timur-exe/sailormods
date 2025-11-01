bbc7cc57c7f1caa47f613bf473fb3e2b6ed3e21f
timur-exe <151888707+timur-exe@users.noreply.github.com>
2025-11-01 23:08:37 +0300

    Update alicegpt.py

--- alicegpt.py
+++ alicegpt.py
@@ -37,7 +37,7 @@ class AliceGPT(loader.Module):
             if "Начинаю творить, вернусь через несколько секунд" in response1.text:
              response2 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
              await utils.answer(message, f" 🤷🏼‍♀️ <b>твой вопрос:</b> \n{text}\n\n💅🏻 <b>ответ Алисы:</b>\n{response2.photo}")
-            await self._client.forward_messages(chat, {response2}, me)
+             await self._client.forward_messages(chat, {response2}, me)
              await response.delete()
              await response1.delete()
              await response2.delete()
