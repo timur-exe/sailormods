330f1f8cfff59829c6f759da5ec498ce0736dc68
timur-exe <151888707+timur-exe@users.noreply.github.com>
2025-11-02 14:14:43 +0300

    Update alicegpt.py

--- alicegpt.py
+++ alicegpt.py
@@ -1,81 +1,91 @@
-from .. import loader, utils
-from telethon.tl.types import Message
-from telethon import functions
-from telethon import events
-from datetime import timedelta
-import random
 version = (1, 0, 0)
 
+# meta developer: @RUIS_VlP, @matubuntu
 
+import random
+from datetime import timedelta
+from telethon import events
+from telethon import functions
+from telethon.tl.types import Message
+from .. import loader, utils
+
+bot = ["@GPTChatRBot", 5989217330]
 bot1 = ["@alice_ya_bot", 8310045254]
-
-
 @loader.tds
-class AliceGPT(loader.Module):
-     """ALICE GPT"""
+class AliceGPTMod(loader.Module):
+    """ChatGPT 3, Gigachat без API ключа и с контекстом. Бот, который используется для запросов: @Gigachat_bot и @GPTChatRBot. Модуль распространяется по лицензии MIT."""
+
+    strings = {
+        "name": "AliceGPT",
+    }
             	
-        @loader.command()
+    @loader.command()
+    async def gptdelcmd(self, message):
+        """- очищает историю переписки с нейросетью(контекст)"""
+        chat = bot[1]
+        text = "/clear"
+        async with message.client.conversation(bot[0]) as conv:
+            response = await conv.send_message(text)
+            response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+            await utils.answer(message, "✅<b>Контекст успешно очищен!</b>")
+            await response.delete()
+            await response1.delete()
+           
+    @loader.command()
     async def alice(self, message):
-        """<текст> - запрос к Алисе"""
+        """<текст> - спросить Алису"""
         chat = bot1[1]
         reply = await message.get_reply_message()
         text = reply.raw_text if reply else message.text[5:]
         if len(text) < 3:
-            await utils.answer(message, "🤦🏼‍♀️ <b>Кошмар!\nСлишком короткий запрос.</b>")
-            return
-            await utils.answer(message, "💅🏻 <b>Алиса думает...</b>")
-            async with message.client.conversation(bot1[1]) as conv:
+         await utils.answer(message, "🚫<b>Ошибка!\nСлишком маленький запрос.</b>")
+         return
+        await utils.answer(message, "🤖<b>Нейросеть обрабатывает ваш запрос...</b>")
+        async with message.client.conversation(bot1[0]) as conv:
             
             response = await conv.send_message(text)
             
             response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
             
-            if "Начинаю творить, вернусь через несколько секунд" in response1.text:
-                response2 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-                        im = await get_image(self, m)
-                        if not im:
-                         return
-                         im.is_webp = not im.is_webp
-                      await go_out(self, m, im, im.image)
-                await response.delete()
-                await response1.delete()
-                await response2.delete()
-                return
-                elif "Рисую, через несколько секунд будет готово" in response1.text:
-                response2 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-                im = await get_image(self, m)
-                        if not im:
-                         return
-                         im.is_webp = not im.is_webp
-                      await go_out(self, m, im, im.image)
-                await response.delete()
-                await response1.delete()
-                await response2.delete()
-                return
-                elif "Дайте мне несколько секунд" in response1.text:
-                response2 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-                im = await get_image(self, m)
-                        if not im:
-                         return
-                         im.is_webp = not im.is_webp
-                      await go_out(self, m, im, im.image){response2}, me)
-                await response.delete()
-                await response1.delete()
-                await response2.delete()
-                return
-                else:
-                await utils.answer(message, f" 🤷🏼‍♀️ <b>твой вопрос:</b> \n{text}\n\n💅🏻 <b>ответ Алисы:</b>\n{response1.text}")
-                await response.delete()
-                await response1.delete()
-                
-                @loader.command()
-                async def aliceclear(self, message):
-                    """- очищает историю переписки с Алисой (контекст)"""
-                    chat = bot1[1]
-                    text = "🧹 Новый диалог"
-                    async with message.client.conversation(bot1[0]) as conv:
-                    response = await conv.send_message(text)
-                    response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-                    await utils.answer(message, "✅<b>Контекст успешно сброшен!</b>")
-                    await response.delete()
-                    await response1.delete()
+            if "💭Ещё чуть-чуть, готовлю ответ" in response1.text:
+             response2 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+             await utils.answer(message, f"❓<b>Вопрос:</b> \n{text}\n\n🤖 <b>Ответ нейросети:</b>\n{response2.text}")
+             await response.delete()
+             await response1.delete()
+             await response2.delete()
+             return
+            else:
+             await utils.answer(message, f"❓<b>Вопрос:</b> \n{text}\n\n🤖 <b>Ответ нейросети:</b>\n{response1.text}")
+             await response.delete()
+             await response1.delete()
+
+
+            	
+    @loader.command()
+    async def aliceclear(self, message):
+        """- очищает историю переписки с нейросетью(контекст)"""
+        chat = bot1[1]
+        text = "🆕 Перезапустить диалог"
+        async with message.client.conversation(bot1[0]) as conv:
+            response = await conv.send_message(text)
+            response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+            await utils.answer(message, "✅<b>Контекст успешно очищен!</b>")
+            await response.delete()
+            await response1.delete()
+           
+    @loader.command()
+    async def gptcmd(self, message):
+        """<текст> - запрос к нейросети ChatGPT"""
+        chat = bot[1]
+        reply = await message.get_reply_message()
+        text = reply.raw_text if reply else message.text[5:]
+        if len(text) < 3:
+        	await utils.answer(message, "🚫<b>Ошибка!\nСлишком маленький запрос.</b>")
+        	return
+        await utils.answer(message, "🤖<b>Нейросеть обрабатывает ваш запрос...</b>")
+        async with message.client.conversation(bot[0]) as conv:
+            response = await conv.send_message(text)
+            response1 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
+            await utils.answer(message, f"❓<b>Вопрос:</b> \n{text}\n\n🤖 <b>Ответ нейросети:</b>\n{response1.text}")
+            await response.delete()
+            await response1.delete()
